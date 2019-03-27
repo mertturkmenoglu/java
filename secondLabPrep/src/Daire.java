8796c93477dbe0314c1b1f5ba2cc41c71bfed2c0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2019-03-27 10:03:30 +0300

    Added user input

--- secondLabPrep/src/Daire.java
+++ secondLabPrep/src/Daire.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Daire {
 
     private int kapiNo;
@@ -6,7 +8,11 @@ public class Daire {
     public Daire(int kapiNo) {
         this.kapiNo = kapiNo;
         odalar = new Oda[3];
-        odalar[0] = new Oda("oturma odasi");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Oda0");
+        String tmp = sc.nextLine();
+        odalar[0] = new Oda(tmp);
+        //odalar[0] = new Oda("oturma odasi");
         odalar[1] = new Oda("yatak odasi");
         odalar[2] = new Oda("mutfak");
     }
