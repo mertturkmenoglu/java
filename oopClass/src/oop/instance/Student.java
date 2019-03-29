fa10737fbcb11140ccfdb46462c6ce47703fcc67
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2019-03-29 06:23:44 +0300

    Instance initialization

---
+++ oopClass/src/oop/instance/Student.java
@@ -0,0 +1,39 @@
+package oop.instance;
+
+// Illustrate instance initialization block
+public class Student {
+
+    private String name;
+    private String id;
+
+    {
+        System.out.println("Instance created");
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+        System.out.println("Student object created");
+    }
+
+    @SuppressWarnings("unused")
+    public String getName() {
+        return name;
+    }
+
+    @SuppressWarnings("unused")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @SuppressWarnings("unused")
+    public String getId() {
+        return id;
+    }
+
+    @SuppressWarnings("unused")
+    public void setId(String id) {
+        this.id = id;
+    }
+}
