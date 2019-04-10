f4023e3649ba52c033dd3824ab8295421e679c6f
Mert <mertturkmenoglu99@gmail.com>
2019-04-11 00:38:49 +0300

    Add Math.random() demo

---
+++ oopClass/src/oop/mathExamples/ex_001/Main.java
@@ -0,0 +1,11 @@
+package oop.mathExamples.ex_001;
+
+// Math.
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            double randomValue = Math.random();
+            System.out.println(randomValue);
+        }
+    }
+}
