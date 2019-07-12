c9d2c6d6074ec20b976b3869f4b88afbdd5d9375
Mert <mertturkmenoglu99@gmail.com>
2019-07-12 22:43:50 +0300

    Add URLConnection class example

---
+++ oop/src/e002_URLConnection/Main.java
@@ -0,0 +1,23 @@
+package e002_URLConnection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/mertturkmenoglu/java/master/oop/src/e001_URL/Main.java");
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            int i;
+
+            while ((i = inputStream.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
