95312a68157001fcea2df2e89f360f73ebe243f2
Mert <mertturkmenoglu99@gmail.com>
2019-07-12 22:50:32 +0300

    Add HttpURLConnection class example

---
+++ oop/src/e003_HttpURLConnection/Main.java
@@ -0,0 +1,22 @@
+package e003_HttpURLConnection;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.google.com/search?q=kotlin&oq=kotlin&sourceid=chrome&ie=UTF-8");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            for (int i = 1; i < 8; i++) {
+                System.out.println(httpURLConnection.getHeaderFieldKey(i) + " = " + httpURLConnection.getHeaderField(i));
+            }
+
+            httpURLConnection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
